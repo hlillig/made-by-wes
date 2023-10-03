28d01ac4baca42ea11765d1850dee28489a3145c
Helen Lillig <htlillig@gmail.com>
2023-10-02 21:03:19 -0400

    Updates

---
+++ component---src-pages-portfolio-js-1dab87b19feb97765180.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[25],{4037:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(5592),c=a(262);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-portfolio-js-1dab87b19feb97765180.js.map
